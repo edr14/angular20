3e09b0570050c8f159bde4c12ff4eedabae86749
Edgar Chávez <ecd.teckay@gmail.com>
2025-10-27 09:16:55 -0600

    integreando firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+
+
+export const environment = {
+  production: false,
+ 
+  firebase: {
+    apiKey: "AIzaSyA3U8uQbgj14ZzoKYhAvF8SKI18pgXl4CI",
+    authDomain: "angular20-fa479.firebaseapp.com",
+    projectId: "angular20-fa479",
+    storageBucket: "angular20-fa479.firebasestorage.app",
+    messagingSenderId: "936747992367",
+    appId: "1:936747992367:web:ba54908d3f399110c18dda"
+  }
+};
